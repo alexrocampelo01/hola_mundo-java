83c03bc3712409eaaaac99d06c0bcaa8e7b45755
alejandro <alejandro@gamil.com>
2021-04-21 10:10:36 +0200

    hola_mundo 1

---
+++ hola_mundo/src/hola_mundo/Hola_mundo.java
@@ -0,0 +1,16 @@
+package hola_mundo;
+
+public class Hola_mundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+		
+		int n=0;
+		if (n == 0) {
+			System.out.println("soy cero");
+		}
+		
+	}
+
+}
